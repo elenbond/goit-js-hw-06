2b5930d6e17c5d44ce607ae483f84562adca863e
Olena Bondiuchna <93439766+elenbond@users.noreply.github.com>
2022-05-19 12:11:59 +0300

    updated task-10

--- js/task-10.js
+++ js/task-10.js
@@ -13,25 +13,41 @@ console.log(buttonDestroy);
 const boxes = document.querySelector("#boxes");
 console.log(boxes);
 
-
-buttonCreate.addEventListener("click", createBoxes);
 let firstDivHeight = 30;
 let firstDivWidth = 30;
 
-// const createBoxes = (amount) => {
-//   amount = inputAmount.value;
-//   for (let i = 0; i < amount; i += 1) {
-//     const createNewBox = document.createElement("div");
-//     if()
+const createBoxes = (amount) => {
+  amount = inputAmount.value;
+  for (let i = 0; i < amount; i += 1) {
+    const createNewDiv = document.createElement("div");
+    if (firstDivHeight > createNewDiv.style.height) {
+      firstDivHeight += 10;
+      firstDivWidth += 10;
+    } else {
+      firstDivHeight = 30;
+      firstDivWidth = 30;
+      const firstCreatedDiv = document.createElement("div");
+      firstCreatedDiv.style.height = `${firstDivHeight} px`;
+      firstCreatedDiv.style.width = `${firstDivWidth} px`;
+      firstCreatedDiv.style.backgroundColor = getRandomHexColor();
+      boxes.append(firstCreatedDiv);
+      console.log(`${firstDivWidth} px`);
+    }
+    createNewDiv.style.height = `${firstDivHeight} px`;
+    createNewDiv.style.width = `${firstDivWidth} px`;
+    createNewDiv.style.backgroundColor = getRandomHexColor();
 
-
-//   }
-
-
-
-//   // const amountOfCreatedBoxes = document.createElement("div") * amount;
-//   // boxes.append(...amountOfCreatedBoxes);
-//   return amountOfCreatedBoxes;
-// }
-// // boxes.append(createBoxes);
-
+    console.log(createNewDiv);
+    console.log(createNewDiv.style.width);
+    // console.log(createNewDiv.previousElementSibling);
+  }
+}
+const destroyBoxes = () => {
+  for (let i = 0; i < boxes.children.length; i += 1) {
+    console.log([i]);
+    boxes.children[i].remove();
+    [i] = [i - 1];
+  }
+}
+buttonDestroy.addEventListener("click", destroyBoxes);
+buttonCreate.addEventListener("click", createBoxes);
